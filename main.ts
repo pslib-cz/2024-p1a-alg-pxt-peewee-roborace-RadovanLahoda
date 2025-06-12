8b033a5ea033f3a4ea966d63a599010811fb3f5f
bomboklada <rada.lahoda08@gmail.com>
2025-06-12 21:21:03 +0200

    UpravaKodu

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
 //let o = 125
 //const stripLenght: number = 36;
 //let pasek = neopixel.create(DigitalPin.P0, stripLenght, NeoPixelMode.RGB)
+
 radio.setGroup(123)
 radio.setFrequencyBand(48)
 type IRC = {
@@ -14,70 +15,66 @@ const IR: IRC = {
     c: DigitalPin.P15,
     r: DigitalPin.P13
 }
-let x = 0
+
 pins.setPull(IR.l, PinPullMode.PullNone);
 pins.setPull(IR.c, PinPullMode.PullNone);
 pins.setPull(IR.r, PinPullMode.PullNone);
 let datal: number;
 let datar: number;
 let datac: number;
-let ein = 0
-let zwei = 0
-let z = 0
-let g = 0
+let lCommand = 0
+let rCommand = 0
+let blackL = 0
+let blackR = 0
 
 basic.forever(function () {
     datal = pins.digitalReadPin(IR.l);
     datac = pins.digitalReadPin(IR.c)
     datar = pins.digitalReadPin(IR.r)
-    radio.onReceivedString(function (ReceivedString) {
-        let dilek = ReceivedString.split(',')
-        ein = parseInt(dilek[0])
-        zwei = parseInt(dilek[1])
-    })
-    if (ein == 1) {
+
+    if (lCommand == 1) {
         if (datar == 0) {
         }
         if (datal == 0) {
             PCAmotor.MotorRun(PCAmotor.Motors.M1, 0)
-            let z = 1
+            blackL = 1
         }
-        if ((datal == 1) && (z == 1)) {
+        if ((datal == 1) && (blackL == 1)) {
             PCAmotor.MotorRun(PCAmotor.Motors.M4, 200)
-            ein = 0
+            lCommand = 0
         }
         basic.pause(20)
     }
-    if (zwei == 1) {
+    if (rCommand == 1) {
         if (datar == 0) {
             PCAmotor.MotorRun(PCAmotor.Motors.M4, 0)
-            g = 1
+            blackR = 1
         }
-        if ((datar == 1) && (g == 1)) {
+        if ((datar == 1) && (blackR == 1)) {
             PCAmotor.MotorRun(PCAmotor.Motors.M4, 200)
-            zwei = 0
+            rCommand = 0
         }
         if (datal == 0) {
+            //idk
         }
         basic.pause(20)
     }
-    if ((zwei == 0) && (ein == 0)) {
-        if (datal === 0) (
+    if ((rCommand == 0) && (lCommand == 0)) {
+        if (datal === 0) {
             PCAmotor.MotorRun(PCAmotor.Motors.M1, 0)
-        )
-        if (datar === 0) (
+        }
+        if (datar === 0) {
             PCAmotor.MotorRun(PCAmotor.Motors.M4, 0)
-        )
-        if (datac === 0) (
+        }
+        if (datac === 0) {
             PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
-            &&
             PCAmotor.MotorRun(PCAmotor.Motors.M4, -200)
-        )
+        }
         basic.pause(20)
     }
-    if ((zwei == 1) && (ein == 1)) {
-        zwei = 0
-        ein = 0
+    if ((rCommand == 1) && (lCommand == 1)) {
+        rCommand = 0
+        lCommand = 0
     }
 //    o += 5
 //    p += 5
@@ -89,4 +86,10 @@ basic.forever(function () {
 //    if (p >= 350) {
 //        p = -0
 //    }
+})
+
+radio.onReceivedString(function (ReceivedString) {
+    let dilek = ReceivedString.split(',')
+    lCommand = parseInt(dilek[0])
+    rCommand = parseInt(dilek[1])
 })
